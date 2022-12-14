881ffec5bc64e50383b6681682c8c02f4f002486
benchanjamin <47726262+benchanjamin@users.noreply.github.com>
2022-12-14 13:26:48 -0500

    working now

--- background.js
+++ background.js
@@ -1,5 +1,28 @@
-chrome.devtools.network.onNavigated.addListener((url) => {
-    chrome.devtools.network.getHAR((log) => {
-        console.log(log)
-    })
+// Listen for any changes to the URL of any tab.
+chrome.tabs.onUpdated.addListener(function(tabId, info, tab) {
+    if (info.status === 'complete') {
+        // Get the URL of the tab that was updated.
+        let url = new URL(tab.url);
+        // hostname returns "developer.chrome.com" in
+        // 'https://developer.chrome.com/docs/extensions/reference/webRequest/#type-OnCompletedOptions'
+        let hostname = url.hostname
+
+        // send to contentScript.js
+        chrome.tabs.sendMessage(tabId, {
+            type: "NEW",
+            hostname: hostname,
+        });
+
+        // Log the URL to the console.
+        console.log(hostname);
+    }
 });
+
+// listen for web requests
+chrome.webRequest.onCompleted.addListener(
+    function(details) {
+        // log the URL of the web request
+        console.log(details);
+    },
+    {urls: ["<all_urls>"]}
+);
